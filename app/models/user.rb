ef8afd62eb71141e4f3fc594db6d5bb725c919a1
Saurabh Prakash <saurabhprakash7777@gmail.com>
2025-04-29 22:49:11 +0530

    Validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :articles
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 30 }
+  VALID_REGEX_INDEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 35 },
+                    format: { with: VALID_REGEX_INDEX }
 end
