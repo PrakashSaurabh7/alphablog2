812c17a66313cf6b93aa78f2d5e7cab031a72397
Saurabh Prakash <saurabhprakash7777@gmail.com>
2025-05-01 01:50:05 +0530

    After configuring signup page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "new" => "articles#new"
   post "add" => "articles#add"
-  root "articles#showAll"
+  get "showAll", to: "articles#showAll"
   get "showById/:id" => "articles#showById"
   get "edit/:id" => "articles#edit"
   patch "update/:id" => "articles#update"
   delete "delete/:id" => "articles#delete"
+
+  # get "signup", to: "users#new"
+  root "users#new"
+  post "user", to: "users#create"
 end
